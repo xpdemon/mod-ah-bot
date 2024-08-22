5189b72de903ea73d38b9f2a73611676d0a31510
Kewin Rausch <kewinrausch@users.noreply.github.com>
2024-08-22 16:39:15 +0200

    Overall cleanup and extension of the mod (#96)
    * Overall cleanup of the mod, separation of class sources, introduction of whitelist mechanism compatible with disabled item table (take precedence), new configuration options to regulate amount of elements sold in the market, their size and the elapsed time
    
    * Fixed a bug which caused the seller to propose stack going over the given limit
    
    * No changes, just the script classes being organized in their own source and header files
    
    * Configuration moved from the bot to its own class; extended the bot to support multiple instances active at once

--- src/cs_ah_bot.cpp
+++ src/cs_ah_bot.cpp
@@ -36,7 +36,10 @@ using namespace Acore::ChatCommands;
 class ah_bot_commandscript : public CommandScript
 {
 public:
-    ah_bot_commandscript() : CommandScript("ah_bot_commandscript") { }
+    ah_bot_commandscript() : CommandScript("ah_bot_commandscript")
+    {
+
+    }
 
     std::vector<ChatCommand> GetCommands() const override
     {
@@ -44,38 +47,118 @@ public:
         {
             { "ahbotoptions", HandleAHBotOptionsCommand, SEC_GAMEMASTER, Console::Yes }
         };
+
         return commandTable;
     }
 
     static bool HandleAHBotOptionsCommand(ChatHandler* handler, const char*args)
     {
         uint32 ahMapID = 0;
-        char* opt = strtok((char*)args, " ");
-        char* ahMapIdStr = strtok(NULL, " ");
+        char*  opt     = strtok((char*)args, " ");
+
+        if (!opt)
+        {
+            handler->PSendSysMessage("Invalid syntax");
+            handler->PSendSysMessage("Try ahbotoptions help to see a list of options.");
+
+            return false;
+        }
+
+        //
+        // Support function the item quality
+        //
 
         auto qualityStringToEnum = [](const char* qualityName, int maxCount)
         {
             if (strncmp(qualityName, "grey", maxCount) == 0)
+            {
                 return ITEM_QUALITY_POOR;
+            }
+
             if (strncmp(qualityName, "white", maxCount) == 0)
+            {
                 return ITEM_QUALITY_NORMAL;
+            }
+
             if (strncmp(qualityName, "green", maxCount) == 0)
+            {
                 return ITEM_QUALITY_UNCOMMON;
+            }
+
             if (strncmp(qualityName, "blue", maxCount) == 0)
+            {
                 return ITEM_QUALITY_RARE;
+            }
+
             if (strncmp(qualityName, "purple", maxCount) == 0)
+            {
                 return ITEM_QUALITY_EPIC;
+            }
+
             if (strncmp(qualityName, "orange", maxCount) == 0)
+            {
                 return ITEM_QUALITY_LEGENDARY;
+            }
+
             if (strncmp(qualityName, "yellow", maxCount) == 0)
+            {
                 return ITEM_QUALITY_ARTIFACT;
+            }
 
             return static_cast<ItemQualities>(-1); // Invalid
         };
 
+        //
+        // Commands which does not requires an AH
+        //
+
+        int l = strlen(opt);
+
+        if (strncmp(opt, "buyer", l) == 0)
+        {
+            char* param1 = strtok(NULL, " ");
+
+            if (!param1)
+            {
+                handler->PSendSysMessage("Syntax is: ahbotoptions buyer $state (0 off 1 on)");
+                return false;
+            }
+
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::buyer, 0, 0, param1);
+            }
+
+            return true;
+        }
+        else if (strncmp(opt, "seller", l) == 0)
+        {
+            char* param1 = strtok(NULL, " ");
+
+            if (!param1)
+            {
+                handler->PSendSysMessage("Syntax is: ahbotoptions seller $state (0 off 1 on)");
+                return false;
+            }
+
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::seller, 0, 0, param1);
+            }
+
+            return true;
+        }
+
+        //
+        // Retrieve the auction house type
+        //
+
+        char* ahMapIdStr = strtok(NULL, " ");
+
         if (ahMapIdStr)
         {
             ahMapID = uint32(strtoul(ahMapIdStr, NULL, 0));
+
             switch (ahMapID)
             {
                 case 2:
@@ -88,33 +171,40 @@ public:
             }
         }
 
+        //
+        // Syntax check
+        //
+
         if (!opt)
         {
-            handler->PSendSysMessage("Syntax is: ahbotoptions $option $ahMapID (2, 6 or 7) $parameter");
+            handler->PSendSysMessage("Invalid syntax");
             handler->PSendSysMessage("Try ahbotoptions help to see a list of options.");
+
             return false;
         }
 
-        int l = strlen(opt);
+        //
+        // Commands that do requires an AH id to be performed
+        //
 
         if (strncmp(opt, "help", l) == 0)
         {
             handler->PSendSysMessage("AHBot commands:");
-            handler->PSendSysMessage("ahexpire");
-            handler->PSendSysMessage("minitems");
-            handler->PSendSysMessage("maxitems");
-            //handler->PSendSysMessage("");
-            //handler->PSendSysMessage("");
-            handler->PSendSysMessage("percentages");
-            handler->PSendSysMessage("minprice");
-            handler->PSendSysMessage("maxprice");
-            handler->PSendSysMessage("minbidprice");
-            handler->PSendSysMessage("maxbidprice");
-            handler->PSendSysMessage("maxstack");
-            handler->PSendSysMessage("buyerprice");
-            handler->PSendSysMessage("bidinterval");
-            handler->PSendSysMessage("bidsperinterval");
-            handler->PSendSysMessage("reload");
+            handler->PSendSysMessage("buyer - enable/disable buyer");
+            handler->PSendSysMessage("seller - enable/disabler seller");
+            handler->PSendSysMessage("ahexpire - remove all bot auctions");
+            handler->PSendSysMessage("minitems - set min auctions");
+            handler->PSendSysMessage("maxitems - set max auctions");
+            handler->PSendSysMessage("percentages - set selling percentages");
+            handler->PSendSysMessage("minprice - set min price");
+            handler->PSendSysMessage("maxprice - set max price");
+            handler->PSendSysMessage("minbidprice - set min bid price for buyer");
+            handler->PSendSysMessage("maxbidprice - set max bid price for buyer");
+            handler->PSendSysMessage("maxstack - set max stack");
+            handler->PSendSysMessage("buyerprice - set the buyer price policy");
+            handler->PSendSysMessage("bidinterval - set the bid interval for buyer");
+            handler->PSendSysMessage("bidsperinterval - set the bid amount for buyer");
+
             return true;
         }
         else if (strncmp(opt, "ahexpire", l) == 0)
@@ -125,41 +215,52 @@ public:
                 return false;
             }
 
-            auctionbot->Commands(AHBotCommand::ahexpire, ahMapID, 0, NULL);
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::ahexpire, ahMapID, 0, NULL);
+            }
         }
         else if (strncmp(opt, "minitems", l) == 0)
         {
             char* param1 = strtok(NULL, " ");
+
             if (!ahMapIdStr || !param1)
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions minitems $ahMapID (2, 6 or 7) $minItems");
                 return false;
             }
 
-            auctionbot->Commands(AHBotCommand::minitems, ahMapID, 0, param1);
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::minitems, ahMapID, 0, param1);
+            }
         }
         else if (strncmp(opt, "maxitems", l) == 0)
         {
             char* param1 = strtok(NULL, " ");
+
             if (!ahMapIdStr || !param1)
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions maxitems $ahMapID (2, 6 or 7) $maxItems");
                 return false;
             }
 
-            auctionbot->Commands(AHBotCommand::maxitems, ahMapID, 0, param1);
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::maxitems, ahMapID, 0, param1);
+            }
         }
         else if (strncmp(opt, "percentages", l) == 0)
         {
-            char* param1 = strtok(NULL, " ");
-            char* param2 = strtok(NULL, " ");
-            char* param3 = strtok(NULL, " ");
-            char* param4 = strtok(NULL, " ");
-            char* param5 = strtok(NULL, " ");
-            char* param6 = strtok(NULL, " ");
-            char* param7 = strtok(NULL, " ");
-            char* param8 = strtok(NULL, " ");
-            char* param9 = strtok(NULL, " ");
+            char* param1  = strtok(NULL, " ");
+            char* param2  = strtok(NULL, " ");
+            char* param3  = strtok(NULL, " ");
+            char* param4  = strtok(NULL, " ");
+            char* param5  = strtok(NULL, " ");
+            char* param6  = strtok(NULL, " ");
+            char* param7  = strtok(NULL, " ");
+            char* param8  = strtok(NULL, " ");
+            char* param9  = strtok(NULL, " ");
             char* param10 = strtok(NULL, " ");
             char* param11 = strtok(NULL, " ");
             char* param12 = strtok(NULL, " ");
@@ -172,38 +273,36 @@ public:
                 handler->PSendSysMessage("1 GreyTradeGoods 2 WhiteTradeGoods 3 GreenTradeGoods 4 BlueTradeGoods 5 PurpleTradeGoods");
                 handler->PSendSysMessage("6 OrangeTradeGoods 7 YellowTradeGoods 8 GreyItems 9 WhiteItems 10 GreenItems 11 BlueItems");
                 handler->PSendSysMessage("12 PurpleItems 13 OrangeItems 14 YellowItems");
-                handler->PSendSysMessage("The total must add up to 100%%");
+
                 return false;
             }
 
-            uint32 greytg = uint32(strtoul(param1, NULL, 0));
-            uint32 whitetg = uint32(strtoul(param2, NULL, 0));
-            uint32 greentg = uint32(strtoul(param3, NULL, 0));
-            uint32 bluetg = uint32(strtoul(param4, NULL, 0));
-            uint32 purpletg = uint32(strtoul(param5, NULL, 0));
-            uint32 orangetg = uint32(strtoul(param6, NULL, 0));
-            uint32 yellowtg = uint32(strtoul(param7, NULL, 0));
-            uint32 greyi = uint32(strtoul(param8, NULL, 0));
-            uint32 whitei = uint32(strtoul(param9, NULL, 0));
-            uint32 greeni = uint32(strtoul(param10, NULL, 0));
-            uint32 bluei = uint32(strtoul(param11, NULL, 0));
-            uint32 purplei = uint32(strtoul(param12, NULL, 0));
-            uint32 orangei = uint32(strtoul(param13, NULL, 0));
-            uint32 yellowi = uint32(strtoul(param14, NULL, 0));
+            uint32 greytg       = uint32(strtoul(param1, NULL, 0));
+            uint32 whitetg      = uint32(strtoul(param2, NULL, 0));
+            uint32 greentg      = uint32(strtoul(param3, NULL, 0));
+            uint32 bluetg       = uint32(strtoul(param4, NULL, 0));
+            uint32 purpletg     = uint32(strtoul(param5, NULL, 0));
+            uint32 orangetg     = uint32(strtoul(param6, NULL, 0));
+            uint32 yellowtg     = uint32(strtoul(param7, NULL, 0));
+            uint32 greyi        = uint32(strtoul(param8, NULL, 0));
+            uint32 whitei       = uint32(strtoul(param9, NULL, 0));
+            uint32 greeni       = uint32(strtoul(param10, NULL, 0));
+            uint32 bluei        = uint32(strtoul(param11, NULL, 0));
+            uint32 purplei      = uint32(strtoul(param12, NULL, 0));
+            uint32 orangei      = uint32(strtoul(param13, NULL, 0));
+            uint32 yellowi      = uint32(strtoul(param14, NULL, 0));
+
             uint32 totalPercent = greytg + whitetg + greentg + bluetg + purpletg + orangetg + yellowtg + greyi + whitei + greeni + bluei + purplei + orangei + yellowi;
 
             if (totalPercent == 0 || totalPercent != 100)
             {
-                handler->PSendSysMessage("Syntax is: ahbotoptions percentages $ahMapID (2, 6 or 7) $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14");
-                handler->PSendSysMessage("1 GreyTradeGoods 2 WhiteTradeGoods 3 GreenTradeGoods 4 BlueTradeGoods 5 PurpleTradeGoods");
-                handler->PSendSysMessage("6 OrangeTradeGoods 7 YellowTradeGoods 8 GreyItems 9 WhiteItems 10 GreenItems 11 BlueItems");
-                handler->PSendSysMessage("12 PurpleItems 13 OrangeItems 14 YellowItems");
                 handler->PSendSysMessage("The total must add up to 100%%");
+
                 return false;
             }
 
-            char param[100];
-            param[0] = '\0';
+            char param[100] = { 0 };
+
             strcat(param, param1);
             strcat(param, " ");
             strcat(param, param2);
@@ -231,7 +330,11 @@ public:
             strcat(param, param13);
             strcat(param, " ");
             strcat(param, param14);
-            auctionbot->Commands(AHBotCommand::percentages, ahMapID, 0, param);
+
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::percentages, ahMapID, 0, param);
+            }
         }
         else if (strncmp(opt, "minprice", l) == 0)
         {
@@ -245,8 +348,14 @@ public:
             }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::minprice, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::minprice, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions minprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $price");
@@ -257,6 +366,7 @@ public:
         {
             char* param1 = strtok(NULL, " ");
             char* param2 = strtok(NULL, " ");
+
             if (!ahMapIdStr || !param1 || !param2)
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions maxprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $price");
@@ -264,8 +374,14 @@ public:
             }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::maxprice, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::maxprice, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions maxprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $price");
@@ -284,6 +400,7 @@ public:
             }
 
             uint32 minBidPrice = uint32(strtoul(param2, NULL, 0));
+
             if (minBidPrice < 1 || minBidPrice > 100)
             {
                 handler->PSendSysMessage("The min bid price multiplier must be between 1 and 100");
@@ -291,8 +408,14 @@ public:
             }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::minbidprice, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::minbidprice, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions minbidprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $price");
@@ -311,6 +434,7 @@ public:
             }
 
             uint32 maxBidPrice = uint32(strtoul(param2, NULL, 0));
+
             if (maxBidPrice < 1 || maxBidPrice > 100)
             {
                 handler->PSendSysMessage("The max bid price multiplier must be between 1 and 100");
@@ -318,8 +442,14 @@ public:
             }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::maxbidprice, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::maxbidprice, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions max bidprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $price");
@@ -345,8 +475,14 @@ public:
             // }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::maxstack, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::maxstack, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions maxstack $ahMapID (2, 6 or 7) $color (grey, white, green, blue, purple, orange or yellow) $value");
@@ -365,8 +501,14 @@ public:
             }
 
             auto quality = qualityStringToEnum(param1, l);
+
             if (quality != static_cast<ItemQualities>(-1))
-                auctionbot->Commands(AHBotCommand::buyerprice, ahMapID, quality, param2);
+            {
+                for (AuctionHouseBot* bot: gBots)
+                {
+                    bot->Commands(AHBotCommand::buyerprice, ahMapID, quality, param2);
+                }
+            }
             else
             {
                 handler->PSendSysMessage("Syntax is: ahbotoptions buyerprice $ahMapID (2, 6 or 7) $color (grey, white, green, blue or purple) $price");
@@ -383,7 +525,10 @@ public:
                 return false;
             }
 
-            auctionbot->Commands(AHBotCommand::bidinterval, ahMapID, 0, param1);
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::bidinterval, ahMapID, 0, param1);
+            }
         }
         else if (strncmp(opt, "bidsperinterval", l) == 0)
         {
@@ -395,15 +540,19 @@ public:
                 return false;
             }
 
-            auctionbot->Commands(AHBotCommand::bidsperinterval, ahMapID, 0, param1);
+            for (AuctionHouseBot* bot: gBots)
+            {
+                bot->Commands(AHBotCommand::bidsperinterval, ahMapID, 0, param1);
+            }
         }
         else
         {
-            handler->PSendSysMessage("Syntax is: ahbotoptions $option $ahMapID (2, 6 or 7) $parameter");
+            handler->PSendSysMessage("Invalid syntax");
             handler->PSendSysMessage("Try ahbotoptions help to see a list of options.");
             return false;
         }
 
+        handler->PSendSysMessage("Done");
         return true;
     }
 };
